7545858aabb0bf48c7327e08903ef7b1e81679b8
Arslan Amanat <arslanamanat75@gmail.com>
2025-02-04 16:21:12 +0500

    3 pages

---
+++ src/app/dataCenter/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DataCenter = () => {
+  return (
+    <div className="w-[15000px] h-[17000px] bg-red overflow-auto bg-[#EDF7FF]">
+    <div className='bg-[#EDF7FF] pl-[990px] pt-[1110px] font-bold'>
+    <div className='text-[#005FA6] text-[580px] w-[7121px] h-[434px]'>Data Centers</div>
+    <div className='text-[#005FA6] text-[580px] w-[6604px] h-[3216px]'>
+
+    </div>
+    <img src="/datacentermap.svg" /> 
+    </div>
+     </div>
+  )
+}
+
+export default DataCenter
---
+++ src/app/tower/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tower = () => {
+  return (
+    <div className="w-[15000px] h-[17000px] bg-red overflow-auto bg-[#EDF7FF]">
+    <div className='bg-[#EDF7FF] pl-[1012px] pt-[1110px] font-bold'>
+    <div className='text-[#005FA6] text-[580px] w-[14954px] h-[16730px]'>Towers</div>
+    <img src="/tower.svg" /> 
+    </div>
+     </div>
+  )
+}
+
+export default Tower
