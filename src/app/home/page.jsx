a219822138ba69a6158b170ed20c2f880efe4aef
Subhan <subhan.suleman@whetstonez.com>
2025-02-04 15:49:14 +0500

    Homepage chnages

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -24,39 +24,64 @@ const HomeMenu = () => {
     }
   };
 
-
   return (
-    <div className="flex justify-between w-full">
+    <div className="flex justify-between w-full h-[100vh] bg-[#050e59]">
       <div className="relative z-10 p-28">
         <ul className="flex flex-col gap-[20px]">
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/terrestrial-cross-borders")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/terrestrial-cross-borders")}
+          >
             <img src="/menuicon/map.svg" /> Terrestrial & Cross Borders
           </li>
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/data-centers")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/data-centers")}
+          >
             <img src="/menuicon/datacenter.svg" /> Data Centers
           </li>
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/landing-stations")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/landing-stations")}
+          >
             <img src="/menuicon/landingstation.svg" /> Landing Stations
           </li>
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/global-points")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/global-points")}
+          >
             <img src="/menuicon/globalpoints.svg" /> Global Points of Presence
           </li>
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/towers")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/towers")}
+          >
             <img src="/menuicon/towers.svg" /> Towers
           </li>
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/equinix-jeddah")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/equinix-jeddah")}
+          >
             <img src="/menuicon/equinix.svg" /> Equinix Jeddah Internet Exchange
           </li>
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/submarine-cables")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/submarine-cables")}
+          >
             <img src="/menuicon/submarine.svg" /> Submarine Cable Systems
           </li>
-          <li className="text-[32px] flex gap-[8px] cursor-pointer" onClick={() => handleMenuClick("/internet-gateways")}>
+          <li
+            className="text-[32px] flex gap-[8px] cursor-pointer"
+            onClick={() => handleMenuClick("/internet-gateways")}
+          >
             <img src="/menuicon/internet.svg" /> Internet Gateways
           </li>
         </ul>
       </div>
-      <div className="absolute top-28 right-3">
-        <img src="/globe.svg" className="" />
+      <div className="absolute right-0 bottom-0">
+        <video autoPlay loop>
+          <source src="/bgvideo.mp4" type="video/mp4" />
+        </video>
       </div>
     </div>
   );
